52e0c58fd03c526ef4cb5e4e9b5f746e2be643af
Dianna Tingg <dtingg@gmail.com>
2019-12-10 14:55:00 -0800

    added snapshot

--- src/components/test/NewPetForm.test.js
+++ src/components/test/NewPetForm.test.js
@@ -3,3 +3,13 @@ import { render, cleanup } from "@testing-library/react"
 import NewPetForm from "../NewPetForm";
 
 
+describe("NewPetForm", () => {
+  test("Snapshot test for NewPetForm", () => {
+    const container = render(
+    <NewPetForm addPetCallback={() => {}}
+    />);
+
+    expect(container.asFragment()).toMatchSnapshot();
+    cleanup();
+  });
+});
